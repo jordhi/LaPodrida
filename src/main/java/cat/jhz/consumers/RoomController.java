5bba2455b4f1b3eb0e21a88a4903199ce8c56bd7
jordhi <jordhi@gmail.com>
2020-04-05 17:04:50 +0200

    trying on bootstrap

--- src/main/java/cat/jhz/consumers/RoomController.java
+++ src/main/java/cat/jhz/consumers/RoomController.java
@@ -1,5 +1,7 @@
 package cat.jhz.consumers;
 
+import cat.jhz.controllers.UserController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,12 +13,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class RoomController {
     public static final String ROOM = "/";
 
+    UserController userController;
+    @Autowired
+    public RoomController(UserController userController) {
+        this.userController = userController;
+    }
+
     @Value("${spring.application.name}")
     String appName;
 
     @GetMapping
     public String index(Model model) {
         model.addAttribute("appName", appName);
+        model.addAttribute("users",userController.readAll() );
         return "index";
     }
 }
